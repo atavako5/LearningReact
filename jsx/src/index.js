18c6f4634310636fdfd910556ebdd7eb532c0568
atavako5 <atavako5@uwo.ca>
2019-05-11 13:15:29 -0400

    learning components

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Import the react and reactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create a react component
+const App = () => {
+  let myText = { text: "Click Me" };
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {myText.text}
+      </button>
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
