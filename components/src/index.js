cd8df692d2d9e4d22a53f07833ef11664c1a8dda
atavako5 <atavako5@uwo.ca>
2019-05-11 20:03:21 -0400

    'Scaffolding App'

--- components/src/index.js
+++ components/src/index.js
@@ -1,30 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import faker from "faker";
 
 const App = () => {
   var myImage = faker.image.avatar();
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        text="Good stuff"
-        image={myImage}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00AM"
-        text="Bad stuff"
-        image={myImage}
-      />
-      <CommentDetail
-        author="Bob"
-        timeAgo="Yesterday at 5:00PM"
-        text="Dumb stuff"
-        image={myImage}
-      />
+      <ApprovalCard>
+        <div>
+          <h4> Warning!!</h4>
+          are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          text="Good stuff"
+          image={myImage}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          text="Bad stuff"
+          image={myImage}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Bob"
+          timeAgo="Yesterday at 5:00PM"
+          text="Dumb stuff"
+          image={myImage}
+        />
+      </ApprovalCard>
     </div>
   );
 };
